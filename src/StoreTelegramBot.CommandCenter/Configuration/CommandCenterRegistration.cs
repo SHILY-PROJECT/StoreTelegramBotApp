15922ff351de157e2208736c3236a4abe846bb4f
Ilya Shimbuev <iliyarock@gmail.com>
2022-09-15 15:51:48 +0300

    Added classes and methods for layer registration

---
+++ src/StoreTelegramBot.CommandCenter/Configuration/CommandCenterRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StoreTelegramBot.CommandCenter.TelegramBot;
+using StoreTelegramBot.CommandCenter.TelegramBot.Commands;
+using StoreTelegramBotApp.Domain.Configuration;
+using StoreTelegramBotApp.Infrastructure.Sql.Configuration;
+
+namespace StoreTelegramBot.CommandCenter.Configuration;
+
+public static class CommandCenterRegistration
+{
+    public static IServiceCollection AddCommandCenter(this IServiceCollection services)
+    {
+        services
+            .AddDomain()
+            .AddInfrastructureSql()
+            .AddScoped<TelegramBotService>()
+            .AddScoped<MenuCommand>();
+
+        return services;
+    }
+}
---
+++ src/StoreTelegramBotApp.Domain/Configuration/DomainRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StoreTelegramBotApp.Domain.Products;
+
+namespace StoreTelegramBotApp.Domain.Configuration;
+
+public static class DomainRegistration
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        services.AddScoped<IProductService, ProductService>();
+
+        return services;
+    }
+}
