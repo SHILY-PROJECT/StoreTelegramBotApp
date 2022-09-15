851c2c9d7385ac0e46440b64751a59fa38136fc8
Ilya Shimbuev <iliyarock@gmail.com>
2022-09-15 17:21:18 +0300

    Added http support tools

---
+++ src/StoreTelegramBotApp.Domain/Toolkit/HttpClientFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Text;
+
+namespace StoreTelegramBot.CommandCenter.Toolkit;
+
+public class HttpClientFactory
+{
+    public static HttpClient CreateDefautl(bool allowAutoRedirect = false) =>
+        CreateHttpClient(new CookieContainer(), allowAutoRedirect);
+
+    public static HttpClient CreateHttpClient(CookieContainer cookieContainer, bool allowAutoRedirect = false)
+    {
+        var httpClientHandler = new HttpClientHandler()
+        {
+            AllowAutoRedirect = allowAutoRedirect,
+            MaxAutomaticRedirections = 5,
+            AutomaticDecompression = DecompressionMethods.All,
+            CookieContainer = cookieContainer
+        };
+
+        var defaultHeaders = new Dictionary<string, string>
+        {
+            { "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" },
+            { "Accept-Encoding", "gzip, deflate, br" },
+            { "Accept-Language", "en-US,en;q=0.9,ru;q=0.8" },
+            { "User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33" },
+            { "Connection", "keep-alive" }
+        };
+
+        var client = new HttpClient(httpClientHandler, false);
+        client.DefaultRequestHeaders.AddRange(defaultHeaders);
+
+        return client;
+    }
+}
---
+++ src/StoreTelegramBotApp.Domain/Toolkit/HttpHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace StoreTelegramBot.CommandCenter.Toolkit;
+
+public static class HttpHelper
+{
+    public static void AddRange(this HttpRequestHeaders requestHeaders, Dictionary<string, string> header, bool skipIfValueEmptyOrNull = false)
+    {
+        foreach (var kv in header)
+        {
+            if (skipIfValueEmptyOrNull && string.IsNullOrWhiteSpace(kv.Value)) continue;
+            requestHeaders.Add(kv.Key, kv.Value);
+        }
+    }
+
+    public static void AddRangeOrReplace(this HttpRequestHeaders requestHeaders, Dictionary<string, string> headers, bool skipIfValueEmptyOrNull = false)
+    {
+        foreach (var kv in headers)
+        {
+            if (skipIfValueEmptyOrNull && string.IsNullOrWhiteSpace(kv.Value)) continue;
+            if (requestHeaders.Contains(kv.Key)) requestHeaders.Remove(kv.Key);
+            requestHeaders.Add(kv.Key, kv.Value);
+        }
+    }
+
+    public static void AddOrReplace(this HttpRequestHeaders requestHeaders, string name, string value, bool skipIfValueEmptyOrNull = false)
+    {
+        if (skipIfValueEmptyOrNull && string.IsNullOrWhiteSpace(value)) return;
+        if (requestHeaders.Contains(name)) requestHeaders.Remove(name);
+        requestHeaders.Add(name, value);
+    }
+
+    public static async Task<string> ExtractAsStringAsync(this HttpContent content)
+    {
+        using var streamReader = new StreamReader(await content.ReadAsStreamAsync(), Encoding.GetEncoding("windows-1251"));
+        return await streamReader.ReadToEndAsync();
+    }
+
+    public static string ExtractAsString(this HttpContent content)
+    {
+        using var streamReader = new StreamReader(content.ReadAsStream(), Encoding.GetEncoding("windows-1251"));
+        return streamReader.ReadToEnd();
+    }
+}
